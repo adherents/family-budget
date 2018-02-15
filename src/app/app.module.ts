66c94bf1250900290fffb53ae8653fc8894919c2
Dima Simonov <keldor80@mail.ru>
2018-02-15 13:36:50 +0200

    templates and module for core features

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AuthModule } from './auth/auth.module';
 import { AppRoutingModule } from './app-routing.module';
 import { UsersService } from './shared/services/users.service';
 import { AuthService } from './shared/services/auth.service';
+import { SystemModule } from './system/system.module';
 
 
 @NgModule({
@@ -17,7 +18,8 @@ import { AuthService } from './shared/services/auth.service';
     BrowserModule,
     HttpModule,
     AuthModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SystemModule
   ],
   providers: [
     UsersService,
