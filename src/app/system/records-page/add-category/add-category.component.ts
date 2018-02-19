8c866e6576eb29bbb12eae09bd5ffb966025706a
Dima Simonov <keldor80@mail.ru>
2018-02-19 16:35:29 +0200

    adding category functionality

--- src/app/system/records-page/add-category/add-category.component.ts
+++ src/app/system/records-page/add-category/add-category.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { CategoriesService } from '../../shared/services/categories.service';
+import { Category } from '../../shared/models/category.model';
 
 @Component({
   selector: 'fb-add-category',
   templateUrl: './add-category.component.html',
   styleUrls: ['./add-category.component.scss']
 })
-export class AddCategoryComponent implements OnInit {
+export class AddCategoryComponent {
+  @Output() onCategoryAdd = new EventEmitter<Category>();
 
-  constructor() { }
+  constructor(private categoriesService: CategoriesService) { }
 
-  ngOnInit() {
+  onSubmit(form: NgForm) {
+    let { name, amount } = form.value;
+    if (amount < 0) amount *= -1;
+
+    const category = new Category(name, amount);
+
+    this.categoriesService.addCategory(category)
+      .subscribe((category: Category) => {
+        form.reset();
+        form.form.patchValue({amount: 1});
+        this.onCategoryAdd.emit(category);
+      });
   }
 
 }
