702c92c5c0f5c03129667d338f7bbf4221259ad1
Dima Simonov <keldor80@mail.ru>
2018-02-19 11:25:37 +0200

    inner logic for bill page

---
+++ src/app/system/bill-page/currency-card/currency-card.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'fb-currency-card',
+  templateUrl: './currency-card.component.html',
+  styleUrls: ['./currency-card.component.scss']
+})
+export class CurrencyCardComponent implements OnInit {
+  @Input() currency: any;
+  currencies: string[] = ['EUR', 'USD'];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
